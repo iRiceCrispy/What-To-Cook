389817b3a2146878acb8a797b8549bbac91b1064
Eric Huang <huangeric.j@gmail.com>
2023-01-05 21:00:43 -0800

    Implement remove ingredients from user ingredient list logic

--- client/src/components/ui/Ingredients/index.jsx
+++ client/src/components/ui/Ingredients/index.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { ingredientsSelectors } from '../../../store/ingredients';
 import { getSessionUser } from '../../../store/session';
-import { addUserIngredients, userIngredientsSelectors } from '../../../store/userIngredients';
+import { addUserIngredients, removeUserIngredients, userIngredientsSelectors } from '../../../store/userIngredients';
 
 const Ingredients = () => {
   const dispatch = useDispatch();
@@ -10,6 +10,7 @@ const Ingredients = () => {
   const ingredientList = useSelector(ingredientsSelectors.selectAll);
   const ingredients = useSelector(userIngredientsSelectors.selectAll);
   const [addList, setAddList] = useState([]);
+  const [removeList, setRemoveList] = useState([]);
   const [input, setInput] = useState('');
   const [edit, setEdit] = useState(false);
 
@@ -17,19 +18,22 @@ const Ingredients = () => {
     && !ingredients.some(i => i.id === il.id)
     && !addList.some(i => i.id === il.id));
 
-  const addIngredients = () => {
-    dispatch(addUserIngredients({ userId: sessionUser.id, ingredients: addList }))
-      .then(() => {
-        setInput('');
-        setAddList([]);
-        setEdit(false);
-      });
-  };
-
-  const cancel = () => {
+  const reset = () => {
     setInput('');
     setAddList([]);
+    setRemoveList([]);
     setEdit(false);
+  };
+
+  const confirm = async () => {
+    if (addList.length) {
+      await dispatch(addUserIngredients({ userId: sessionUser.id, ingredients: addList }));
+    }
+    if (removeList.length) {
+      await dispatch(removeUserIngredients({ userId: sessionUser.id, ingredients: removeList }));
+    }
+
+    reset();
   };
 
   return (
@@ -75,30 +79,67 @@ const Ingredients = () => {
               </li>
             ))}
           </ul>
+          <ul className="removeList">
+            To be removed:
+            {removeList.map(item => (
+              <li
+                className="item"
+                key={item.id}
+                role="menuitem"
+                tabIndex={0}
+                onClick={() => setRemoveList(prev => prev.filter(i => i.name !== item.name))}
+              >
+                <span>{item.name}</span>
+                {' '}
+                {item.verified && <span>✔️</span>}
+                {' '}
+                <span> +</span>
+              </li>
+            ))}
+          </ul>
         </>
       )}
       <ul className="ingredientList">
-        {ingredients.map(ingredient => (
-          <li className="ingredient" key={ingredient.id}>
-            <span>{ingredient.name}</span>
-            {' '}
-            {ingredient.verified && <span>✔️</span>}
-          </li>
-        ))}
+        My ingredients:
+        {edit ? (
+          ingredients.filter(i => !removeList.some(r => r.id === i.id)).map(ingredient => (
+            <li
+              className="option"
+              key={ingredient.id}
+              role="menuitem"
+              tabIndex={0}
+              onClick={() => setRemoveList(prev => [...prev, ingredient])}
+            >
+              <span>{ingredient.name}</span>
+              {' '}
+              {ingredient.verified && <span>✔️</span>}
+              {' '}
+              <span> -</span>
+            </li>
+          ))
+        ) : (
+          ingredients.map(ingredient => (
+            <li className="ingredient" key={ingredient.id}>
+              <span>{ingredient.name}</span>
+              {' '}
+              {ingredient.verified && <span>✔️</span>}
+            </li>
+          ))
+        )}
       </ul>
       {edit ? (
         <>
           <button
             className="btn"
             type="button"
-            onClick={addIngredients}
+            onClick={confirm}
           >
             Confirm
           </button>
           <button
             className="btn"
             type="button"
-            onClick={cancel}
+            onClick={reset}
           >
             Cancel
           </button>
