6a467edbd087bcc9667a6577b89425148985c12a
Eric Huang <huangeric.j@gmail.com>
2023-02-07 01:03:28 -0800

    Add delete recipe function

--- client/src/components/ui/RecipeDetails/index.jsx
+++ client/src/components/ui/RecipeDetails/index.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
-import { recipesSelectors } from '../../../store/recipes';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import { recipesSelectors, removeRecipe } from '../../../store/recipes';
 import { getSessionUser } from '../../../store/session';
 import Navigation from '../Navigation';
 
 const RecipeDetails = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { id } = useParams();
   const sessionUser = useSelector(getSessionUser);
   const recipe = useSelector(state => recipesSelectors.selectById(state, id));
   const isOwner = recipe.user.id === sessionUser?.id;
+
+  const handleDelete = () => {
+    dispatch(removeRecipe(id));
+    navigate('/dashboard');
+  };
 
   return (
     <div id="recipeDetails">
@@ -18,7 +25,13 @@ const RecipeDetails = () => {
         <>
           <h2 className="name">{recipe.name}</h2>
           <p className="description">{recipe.description || 'No description.'}</p>
-          {isOwner && <Link to="./edit" className="btn">Edit</Link>}
+          {isOwner && (
+          <>
+            <Link to="./edit" className="btn">Edit</Link>
+            <button className="btn" type="button" onClick={handleDelete}>Delete</button>
+          </>
+          )}
+
           <ul className="ingredients">
             {recipe.ingredients.map(ingredient => (
               <li className="ingredient" key={ingredient.id}>
