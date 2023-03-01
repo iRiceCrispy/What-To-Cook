9b827d52c9f2e86d1aa5d449adabc3509d28eff1
Eric Huang <huangeric.j@gmail.com>
2023-03-01 07:16:23 -0800

    Containerize with docker-compose and nginx

--- server/app/config.py
+++ server/app/config.py
@@ -3,5 +3,5 @@ from os import environ
 
 class Config:
     SECRET_KEY = environ.get('SECRET_KEY') or 'supersecretkey'
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{environ.get('DB_USERNAME')}:{environ.get('DB_PASSWORD')}@localhost/{environ.get('DB_DATABASE')}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{environ.get('DB_USERNAME')}:{environ.get('DB_PASSWORD')}@{environ.get('DB_HOST')}/{environ.get('DB_DATABASE')}"
     SQLALCHEMY_ECHO = environ.get('SQLALCHEMY_ECHO') or False
